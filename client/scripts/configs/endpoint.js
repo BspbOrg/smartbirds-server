395a5801a54bf48308fd08b615fd3aa5b8b0ada5
Geno Roupsky <geno@roupsky.name>
2015-11-21 22:26:17 +0200

    fix api endpoint

--- client/scripts/configs/endpoint.js
+++ client/scripts/configs/endpoint.js
@@ -1,2 +1,10 @@
+var app = require('../app');
 
-require('../app').constant('ENDPOINT_URL', 'http://'+window.location.hostname+':5000/api');
+switch (process.env.NODE_ENV || 'development') {
+  case 'development':
+    app.constant('ENDPOINT_URL', 'http://'+window.location.hostname+':5000/api');
+    break;
+  default:
+    app.constant('ENDPOINT_URL', '/api');
+    break;
+}
