6d68a5fea580d054eb596b164139a7a3d60fd8e8
Daniel Petrov <dani@masconsult.eu>
2016-01-12 10:09:41 +0200

    renamed fixtures files in order to be executed in given order

---
+++ test/fixtures/05-formCBM.js
@@ -0,0 +1,262 @@
+/**
+ * Created by dani on 11.01.16.
+ */
+
+module.exports = [
+  {
+    model: 'formCBM',
+    data: {
+      zone: {id : 'freeZonePlovdiv'},
+      user: {email: 'user@smartbirds.com'},
+      plot: {
+        type: 'cbm_sector',
+        slug: '1',
+      },
+      visit: {
+        type: 'cbm_visit_number',
+        slug: 'e-early-visit',
+      },
+      secondaryHabitat: {
+        type: 'cbm_habitat',
+        slug: 'a-1-broadleaved-woodland',
+      },
+      primaryHabitat: {
+        type: 'cbm_habitat',
+        slug: 'a-2-coniferous-woodland',
+      },
+      count: 10,
+      distance: {
+        type: 'cbm_distance',
+        slug: '3-over-100-m',
+      },
+      species: {
+        type: 'birds_name',
+        slug: 'accipiter-nisus',
+      },
+      notes: 'Some test notes',
+      visibility: 5.5,
+      mto: 'pretty nice weather',
+      cloudiness: {
+        type: 'main_cloud_level',
+        slug: '33-66',
+      },
+      cloudsType: 'Light grey clouds',
+      windDirection: {
+        type: 'main_wind_direction',
+        slug: 'ene',
+      },
+      windSpeed: {
+        type: 'main_wind_force',
+        slug: '2-light-breeze',
+      },
+      temperature: 24.3,
+      rain: {
+        type: 'main_rain',
+        slug: 'drizzle',
+      },
+      observers: 'Some test observers',
+      endTime: '10:15',
+      endDate: '10/12/2015',
+      startTime: '08:10',
+      startDate: '09/12/2015',
+      source: {
+        type: 'main_source',
+        slug: 'common-bird-monitoring',
+      },
+      latitude: 42.1463749,
+      longitude: 24.7492006
+    }
+  },
+  {
+    model: 'formCBM',
+    data: {
+      zone: {id : 'freeZonePlovdiv'},
+      user: {email: 'admin@smartbirds.com'},
+      plot: {
+        type: 'cbm_sector',
+        slug: '1',
+      },
+      visit: {
+        type: 'cbm_visit_number',
+        slug: 'e-early-visit',
+      },
+      secondaryHabitat: {
+        type: 'cbm_habitat',
+        slug: 'a-1-broadleaved-woodland',
+      },
+      primaryHabitat: {
+        type: 'cbm_habitat',
+        slug: 'a-2-coniferous-woodland',
+      },
+      count: 10,
+      distance: {
+        type: 'cbm_distance',
+        slug: '3-over-100-m',
+      },
+      species: {
+        type: 'birds_name',
+        slug: 'accipiter-nisus',
+      },
+      notes: 'Some test notes',
+      visibility: 5.5,
+      mto: 'pretty nice weather',
+      cloudiness: {
+        type: 'main_cloud_level',
+        slug: '33-66',
+      },
+      cloudsType: 'Light grey clouds',
+      windDirection: {
+        type: 'main_wind_direction',
+        slug: 'ene',
+      },
+      windSpeed: {
+        type: 'main_wind_force',
+        slug: '2-light-breeze',
+      },
+      temperature: 24.3,
+      rain: {
+        type: 'main_rain',
+        slug: 'drizzle',
+      },
+      observers: 'Some test observers',
+      endTime: '10:15',
+      endDate: '10/12/2015',
+      startTime: '08:10',
+      startDate: '09/12/2015',
+      source: {
+        type: 'main_source',
+        slug: 'common-bird-monitoring',
+      },
+      latitude: 42.1463749,
+      longitude: 24.7492006
+    }
+  },
+  {
+    model: 'formCBM',
+    data: {
+      zone: {id : 'freeZonePlovdiv'},
+      user: {email: 'admin@smartbirds.com'},
+      plot: {
+        type: 'cbm_sector',
+        slug: '1',
+      },
+      visit: {
+        type: 'cbm_visit_number',
+        slug: 'e-early-visit',
+      },
+      secondaryHabitat: {
+        type: 'cbm_habitat',
+        slug: 'a-1-broadleaved-woodland',
+      },
+      primaryHabitat: {
+        type: 'cbm_habitat',
+        slug: 'a-2-coniferous-woodland',
+      },
+      count: 10,
+      distance: {
+        type: 'cbm_distance',
+        slug: '3-over-100-m',
+      },
+      species: {
+        type: 'birds_name',
+        slug: 'accipiter-nisus',
+      },
+      notes: 'Some test notes',
+      visibility: 5.5,
+      mto: 'pretty nice weather',
+      cloudiness: {
+        type: 'main_cloud_level',
+        slug: '33-66',
+      },
+      cloudsType: 'Light grey clouds',
+      windDirection: {
+        type: 'main_wind_direction',
+        slug: 'ene',
+      },
+      windSpeed: {
+        type: 'main_wind_force',
+        slug: '2-light-breeze',
+      },
+      temperature: 24.3,
+      rain: {
+        type: 'main_rain',
+        slug: 'drizzle',
+      },
+      observers: 'Some test observers',
+      endTime: '10:15',
+      endDate: '10/12/2015',
+      startTime: '08:10',
+      startDate: '09/12/2015',
+      source: {
+        type: 'main_source',
+        slug: 'common-bird-monitoring',
+      },
+      latitude: 42.1463749,
+      longitude: 24.7492006
+    }
+  },
+  {
+    model: 'formCBM',
+    data: {
+      zone: {id : 'freeZoneSofia'},
+      user: {email: 'admin@smartbirds.com'},
+      plot: {
+        type: 'cbm_sector',
+        slug: '1',
+      },
+      visit: {
+        type: 'cbm_visit_number',
+        slug: 'e-early-visit',
+      },
+      secondaryHabitat: {
+        type: 'cbm_habitat',
+        slug: 'a-1-broadleaved-woodland',
+      },
+      primaryHabitat: {
+        type: 'cbm_habitat',
+        slug: 'a-2-coniferous-woodland',
+      },
+      count: 10,
+      distance: {
+        type: 'cbm_distance',
+        slug: '3-over-100-m',
+      },
+      species: {
+        type: 'birds_name',
+        slug: 'accipiter-nisus',
+      },
+      notes: 'Some test notes',
+      visibility: 5.5,
+      mto: 'pretty nice weather',
+      cloudiness: {
+        type: 'main_cloud_level',
+        slug: '33-66',
+      },
+      cloudsType: 'Light grey clouds',
+      windDirection: {
+        type: 'main_wind_direction',
+        slug: 'ene',
+      },
+      windSpeed: {
+        type: 'main_wind_force',
+        slug: '2-light-breeze',
+      },
+      temperature: 24.3,
+      rain: {
+        type: 'main_rain',
+        slug: 'drizzle',
+      },
+      observers: 'Some test observers',
+      endTime: '10:15',
+      endDate: '10/12/2015',
+      startTime: '08:10',
+      startDate: '09/12/2015',
+      source: {
+        type: 'main_source',
+        slug: 'common-bird-monitoring',
+      },
+      latitude: 42.1463749,
+      longitude: 24.7492006
+    }
+  },
+];
