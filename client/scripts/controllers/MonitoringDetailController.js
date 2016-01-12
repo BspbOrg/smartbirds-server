a4d2f3232399d30d307488fb63cf1d77b32cfa6a
Geno Roupsky <geno@roupsky.name>
2016-01-12 17:54:38 +0200

    fix display of single-choice fields

--- client/scripts/controllers/MonitoringDetailController.js
+++ client/scripts/controllers/MonitoringDetailController.js
@@ -2,25 +2,30 @@
  * Created by groupsky on 11.01.16.
  */
 
-require('../app').controller('MonitoringDetailController', /*@ngInject*/function ($scope, $stateParams, FormCBM, ngToast) {
+require('../app').controller('MonitoringDetailController', /*@ngInject*/function ($scope, $state, $stateParams, $q, FormCBM, ngToast) {
 
   var controller = this;
 
   controller.data = $stateParams.id ? FormCBM.get({id: $stateParams.id}) : new FormCBM();
 
   controller.save = function () {
-    controller.data.$save().then(function (){
+    controller.data.$save().then(function (res){
       $scope.cbmform.$setPristine();
-    }).then(function () {
+      return res;
+    }).then(function (res) {
       ngToast.create({
         className: 'success',
         content: "Save success."
       });
+      return res;
     }, function (error) {
       ngToast.create({
         className: 'danger',
         content: '<p>Could not save!</p><pre>' + (error && error.data && error.data.error || JSON.stringify(error, null, 2)) + '</pre>'
       });
+      return $q.reject(error);
+    }).then(function(res) {
+      $state.go('^.detail', {id: res.id}, {location: 'replace'});
     });
   };
 });
--- client/scripts/directives/field.js
+++ client/scripts/directives/field.js
@@ -4,7 +4,7 @@
 var angular = require('angular');
 var moment = require('moment');
 
-require('../app').directive('field', /*@ngInject*/function() {
+require('../app').directive('field', /*@ngInject*/function () {
   return {
     restrict: 'AE',
     scope: {
@@ -19,21 +19,24 @@ require('../app').directive('field', /*@ngInject*/function() {
     templateUrl: function ($element, $attrs) {
       return '/views/fields/' + ($attrs.type || 'general') + '.html';
     },
-    link: function($scope, $element, $attrs, formCtrl) {
+    link: function ($scope, $element, $attrs, formCtrl) {
       $scope.form = formCtrl;
     },
     controllerAs: 'field',
-    controller: /*@ngInject*/function($scope, $attrs, $filter, Nomenclature) {
+    controller: /*@ngInject*/function ($scope, $attrs, $filter, Nomenclature) {
       var field = this;
 
-      field.form = $scope.form;
+      $scope.$watch('form', function (form) {
+        field.form = form;
+      });
       field.type = $attrs.type;
       field.required = angular.isDefined($attrs.required);
 
       switch ($attrs.type) {
         case 'date':
-        case 'time': {
-          $scope.$watch('field.model', function() {
+        case 'time':
+        {
+          $scope.$watch('field.model', function () {
             if (angular.isString(field.model)) {
               field.model = moment(field.model).toDate();
             }
@@ -41,57 +44,18 @@ require('../app').directive('field', /*@ngInject*/function() {
           break;
         }
         case 'single-choice':
-        case 'multiple-choice': {
+        case 'multiple-choice':
+        {
           var filter = $filter('filter');
           var limitTo = $filter('limitTo');
           field.type = $attrs.nomenclature;
           field.values = Nomenclature.query({type: field.type});
 
-          $scope.$watch('field.model', function() {
-            if (field.model) {
-              if (!field.value || field.value.slug != field.model.slug) {
-                field.value = angular.copy(field.model);
-                field.value.prototype = Nomenclature.prototype;
-                field.values.$promise.then(function(models) {
-                  field.value = undefined;
-                  models.every(function(model){
-                    if (model.slug === field.model.slug) {
-                      field.value = model;
-                      return false;
-                    }
-                    return true;
-                  });
-                });
-              }
-            } else {
-              field.value = undefined;
+          $scope.$watch('field.model', function () {
+            if (field.model && !(field.model instanceof Nomenclature)) {
+              field.model = new Nomenclature(field.model);
             }
           });
-
-          field.getModels = function (q) {
-            //return field.values.$promise.then(function(models){
-              return limitTo(filter(field.values, q), 25);
-            //});
-          };
-
-          field.confirm = function() {
-            field.model = field.value;
-
-            if (field.select)
-              $timeout(function(){
-                field.select();
-              });
-          };
-
-          field.cancel = function() {
-            if (field.onCancel)
-              field.onCancel();
-          };
-
-          field.onSelect = function() {
-            if (field.onConfirm) return;
-            field.onConfirm();
-          };
 
           break;
         }
