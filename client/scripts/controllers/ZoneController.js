afa802094d7cecb3b6220cc8fba17458425c5b6b
Geno Roupsky <geno@roupsky.name>
2016-01-13 15:41:40 +0200

    print button

--- client/scripts/controllers/ZoneController.js
+++ client/scripts/controllers/ZoneController.js
@@ -2,8 +2,12 @@
  * Created by groupsky on 20.11.15.
  */
 
-require('../app').controller('ZoneController', function($scope, $stateParams, Zone) {
+require('../app').controller('ZoneController', function($scope, $stateParams, $window, Zone) {
   var controller = this;
 
   $scope.zone = Zone.get({id: $stateParams.id});
+
+  controller.print = function() {
+    $window.print();
+  }
 });
