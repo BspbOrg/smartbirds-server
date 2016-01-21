8809d8fb4f58f423d710259ab7bbfc36aa3b6055
Geno Roupsky <geno@roupsky.name>
2016-01-21 17:43:35 +0100

    dashboard has add new monitoring button

--- client/scripts/controllers/DashboardController.js
+++ client/scripts/controllers/DashboardController.js
@@ -2,11 +2,17 @@
  * Created by groupsky on 21.01.16.
  */
 
-require('../app').controller('DashboardController', /*@ngInject*/function(Zone){
+require('../app').controller('DashboardController', /*@ngInject*/function(Zone, user){
   var vc = this;
 
   vc.pendingZones = Zone.query({
     status: 'requested',
     limit: 10,
+  });
+
+  vc.ownedZones = Zone.query({
+    status: 'owned',
+    limit: 100,
+    owner: user.getIdentity().id,
   });
 });
