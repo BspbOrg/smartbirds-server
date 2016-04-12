76195bf7b695e32e0d9205fcf653bddf5e26727d
Geno Roupsky <geno@roupsky.name>
2016-04-12 18:49:39 +0300

    round to 5 km coords and use only cbm data

--- client/scripts/directives/homeMap.js
+++ client/scripts/directives/homeMap.js
@@ -29,15 +29,9 @@ require('../app').directive('homeMap', /*@ngInject*/function () {
       });
 
       api.stats.homepage().then(function (zones) {
-        angular.forEach(zones, function (zone) {
-          zone.coordinates = [
-            {latitude: zone.lat1, longitude: zone.lon1},
-            {latitude: zone.lat2, longitude: zone.lon2},
-            {latitude: zone.lat3, longitude: zone.lon3},
-            {latitude: zone.lat4, longitude: zone.lon4}
-          ];
-          zone.center = Zone.prototype.getCenter.apply(zone);
-          vc.zones.push(zone);
+        vc.zones = zones;
+        angular.forEach(zones, function(zone) {
+          zone.id = zone.id || (''+zone.latitude+zone.longitude);
         });
       });
     },
--- server/models/statistics.js
+++ server/models/statistics.js
@@ -1,23 +1,14 @@
 'use strict';
 module.exports = function (sequelize, DataTypes) {
   var Statistics = sequelize.define('Statistics', {
-    id: {
-      type: DataTypes.STRING(10),
-      primaryKey: true
-    },
-    lat1: DataTypes.DOUBLE,
-    lon1: DataTypes.DOUBLE,
-    lat2: DataTypes.DOUBLE,
-    lon2: DataTypes.DOUBLE,
-    lat3: DataTypes.DOUBLE,
-    lon3: DataTypes.DOUBLE,
-    lat4: DataTypes.DOUBLE,
-    lon4: DataTypes.DOUBLE,
+    latitude: DataTypes.DOUBLE,
+    longitude: DataTypes.DOUBLE,
     species_count: DataTypes.INTEGER,
     units_count: DataTypes.INTEGER
   }, {
     timestamps: false,
     tableName: 'cbm_stats'
   });
+  Statistics.removeAttribute('id');
   return Statistics;
 };
