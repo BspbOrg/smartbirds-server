2708a1df3915d5c4b9bedf9698d687142dc81650
Geno Roupsky <geno@roupsky.name>
2016-08-29 09:28:24 +0300

    fix migration for species unique indexes

--- server/migrations/20160828221223-species-not-unq-labelEnBg.js
+++ server/migrations/20160828221223-species-not-unq-labelEnBg.js
@@ -4,30 +4,40 @@ var tableName = "Species";
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.removeIndex(tableName, 'species_type_label_bg')
+    return queryInterface.removeIndex(tableName, "species_type_label_bg")
       .then(function () {
-        return queryInterface.removeIndex(tableName, 'species_type_label_en')
-          .then(function () {
-            return queryInterface.addIndex(tableName, ['labelBg'])
-              .then(function () {
-                return queryInterface.addIndex(tableName, ['labelEn']);
-              })
-          })
-      });
+        return queryInterface.addIndex(tableName, {
+          "unique": false,
+          "fields": ["type", "labelBg"]
+        });
+      })
+      .then(function () {
+        return queryInterface.removeIndex(tableName, "species_type_label_en");
+      })
+      .then(function () {
+        return queryInterface.addIndex(tableName, {
+          "unique": false,
+          "fields": ["type", "labelEn"]
+        });
+      })
   },
 
   down: function (queryInterface, Sequelize) {
-    return queryInterface.removeIndex(tableName, ['labelBg'])
+    return queryInterface.removeIndex(tableName, "species_type_label_bg")
       .then(function () {
-        return queryInterface.removeIndex(tableName, ['labelEn'])
-          .then(function () {
-            return queryInterface.addIndex(tableName, ['labelBg'],
-              { indexName: 'species_type_label_bg', indicesType: 'UNIQUE' })
-          })
-          .then(function () {
-            return queryInterface.addIndex(tableName, ['labelEn'],
-              { indexName: 'species_type_label_en', indicesType: 'UNIQUE' });
-          });
-      });
+        return queryInterface.addIndex(tableName, {
+          "unique": true,
+          "fields": ["type", "labelBg"]
+        });
+      })
+      .then(function () {
+        return queryInterface.removeIndex(tableName, "species_type_label_en");
+      })
+      .then(function () {
+        return queryInterface.addIndex(tableName, {
+          "unique": true,
+          "fields": ["type", "labelEn"]
+        });
+      })
   }
 };
