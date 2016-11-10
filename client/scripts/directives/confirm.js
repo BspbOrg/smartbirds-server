490b28b2de0635230830c6ce92df5caafabdc7e9
Geno Roupsky <geno@roupsky.name>
2016-11-10 15:52:28 +0200

    confirm delete of records

---
+++ client/scripts/directives/confirm.js
@@ -0,0 +1,36 @@
+require('../app').directive('confirm', /*@ngInject*/function () {
+  return {
+    priority: -10,
+    require: 'confirm',
+    link: function ($scope, $element, $attrs, $ctrl) {
+      $element.on('click', $ctrl.confirm)
+    },
+    controller: /*@ngInject*/function ($attrs, $element, $scope, $uibModal) {
+      var $ctrl = this;
+
+      $ctrl.confirm = function (event) {
+        if (event.confirmed) return;
+
+        event.preventDefault();
+        event.stopImmediatePropagation();
+
+        var modalScope = $scope.$new(true);
+        modalScope.message = $attrs.confirm;
+
+        var modalInstance = $uibModal.open({
+          ariaLabelledBy: 'modal-title',
+          ariaDescribedBy: 'modal-body',
+          templateUrl: '/views/system/confirm-dialog.html',
+          size: 'sm',
+          scope: modalScope,
+        });
+
+        modalInstance.result.then(function () {
+          setTimeout(function(){
+            $element.triggerHandler({ type: 'click', confirmed: true });
+          });
+        });
+      }
+    },
+  }
+});
