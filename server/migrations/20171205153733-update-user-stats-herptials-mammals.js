faf4220a108813cfb97fb5b4b2665f7208b82ce1
Geno Roupsky <geno@roupsky.name>
2017-12-05 15:43:48 +0200

    Update user_stats view to include data from herptials and mammals forms

---
+++ server/migrations/20171205153733-update-user-stats-herptials-mammals.js
@@ -0,0 +1,107 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    if (queryInterface.sequelize.options.dialect !== 'postgres') return;
+    return queryInterface.sequelize
+      .query('DROP VIEW IF EXISTS user_stats')
+      .then(function () {
+        return queryInterface.sequelize
+          .query('CREATE VIEW user_stats (' +
+            'id, species_count, entry_count, first_name, last_name' +
+            ') AS ' +
+            ' SELECT ' +
+            '   users.id as id,' +
+            '   coalesce(cbm.species_count, 0) + coalesce(birds.species_count, 0) + coalesce(herps.species_count, 0) + coalesce(herptials.species_count, 0) + coalesce(mammals.species_count, 0) AS species_count,' +
+            '   coalesce(cbm.count, 0) + coalesce(birds.count, 0) + coalesce(herps.count, 0) + coalesce(herptials.count, 0) + coalesce(mammals.count, 0) AS entry_count,' +
+            '   users."firstName" as first_name,' +
+            '   users."lastName" as last_name' +
+            ' ' +
+            ' FROM "Users" users' +
+            ' LEFT JOIN (' +
+            '   SELECT ' +
+            '     "userId" as id,' +
+            '     count(DISTINCT species) as species_count,' +
+            '     count(*) as count' +
+            '   FROM "FormCBM" as cbm' +
+            '   GROUP BY "userId"' +
+            ') cbm USING (id)' +
+            ' LEFT JOIN (' +
+            '   SELECT ' +
+            '     "userId" as id,' +
+            '     count(DISTINCT species) as species_count,' +
+            '     count(*) as count' +
+            '   FROM "FormBirds" as birds' +
+            '   GROUP BY "userId"' +
+            ') birds USING (id)' +
+            ' LEFT JOIN (' +
+            '   SELECT ' +
+            '     "userId" as id,' +
+            '     count(DISTINCT species) as species_count,' +
+            '     count(*) as count' +
+            '   FROM "FormHerps" as herps' +
+            '   GROUP BY "userId"' +
+            ') herps USING (id)' +
+            ' LEFT JOIN (' +
+            '   SELECT ' +
+            '     "userId" as id,' +
+            '     count(DISTINCT species) as species_count,' +
+            '     count(*) as count' +
+            '   FROM "FormHerptials" as herptials' +
+            '   GROUP BY "userId"' +
+            ') herptials USING (id)' +
+            ' LEFT JOIN (' +
+            '   SELECT ' +
+            '     "userId" as id,' +
+            '     count(DISTINCT species) as species_count,' +
+            '     count(*) as count' +
+            '   FROM "FormMammals" as mammals' +
+            '   GROUP BY "userId"' +
+            ') mammals USING (id)' +
+            '');
+      })
+  },
+
+  down: function (queryInterface, Sequelize) {
+    if (queryInterface.sequelize.options.dialect !== 'postgres') return;
+    return queryInterface.sequelize.query('DROP VIEW IF EXISTS user_stats')
+      .then(function () {
+        return queryInterface.sequelize.query('CREATE VIEW user_stats (' +
+          'id, species_count, entry_count, first_name, last_name' +
+          ') AS ' +
+          ' SELECT ' +
+          '   users.id as id,' +
+          '   coalesce(cbm.species_count, 0) + coalesce(birds.species_count, 0) + coalesce(herps.species_count, 0) AS species_count,' +
+          '   coalesce(cbm.count, 0) + coalesce(birds.count, 0) + coalesce(herps.count, 0) AS entry_count,' +
+          '   users."firstName" as first_name,' +
+          '   users."lastName" as last_name' +
+          ' ' +
+          ' FROM "Users" users' +
+          ' LEFT JOIN (' +
+          '   SELECT ' +
+          '     "userId" as id,' +
+          '     count(DISTINCT species) as species_count,' +
+          '     count(*) as count' +
+          '   FROM "FormCBM" as cbm' +
+          '   GROUP BY "userId"' +
+          ') cbm USING (id)' +
+          ' LEFT JOIN (' +
+          '   SELECT ' +
+          '     "userId" as id,' +
+          '     count(DISTINCT species) as species_count,' +
+          '     count(*) as count' +
+          '   FROM "FormBirds" as birds' +
+          '   GROUP BY "userId"' +
+          ') birds USING (id)' +
+          ' LEFT JOIN (' +
+          '   SELECT ' +
+          '     "userId" as id,' +
+          '     count(DISTINCT species) as species_count,' +
+          '     count(*) as count' +
+          '   FROM "FormHerps" as herps' +
+          '   GROUP BY "userId"' +
+          ') herps USING (id)' +
+          '');
+      })
+  }
+};
