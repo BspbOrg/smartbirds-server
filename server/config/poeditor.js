63b0bb01fbdfcf70ec741f3f4e76f458defc0232
Geno Roupsky <geno@roupsky.name>
2017-12-08 12:04:03 +0200

    Use env for poeditor config and fallback to local files

---
+++ server/config/poeditor.js
@@ -0,0 +1,12 @@
+var path = require('path')
+
+exports['default'] = {
+  poeditor: function (api) {
+    return {
+      enabled: process.env.POEDITOR_API_TOKEN,
+      api_token: process.env.POEDITOR_API_TOKEN,
+      project_id: process.env.POEDITOR_PROJECT_ID || 146255,
+      fallbackPath: path.join(__dirname, '/../../i18n')
+    }
+  }
+}
