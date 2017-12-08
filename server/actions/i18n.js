41eab24d5f596c79d66499c6614faaafe341a302
Geno Roupsky <geno@roupsky.name>
2017-12-08 12:04:03 +0200

    Use env for poeditor config and fallback to local files

--- server/actions/i18n.js
+++ server/actions/i18n.js
@@ -1,36 +1,49 @@
+var path = require('path')
+var fs = require('fs')
 var fetch = require('node-fetch')
-var FormData = require('form-data');
+var FormData = require('form-data')
+
+function serveStatic(api, data, next) {
+  fs.readFile(path.join(api.config.poeditor.fallbackPath, data.params.lang+'.json'), 'utf8', function (err, input) {
+    if (err) return next(err)
+    try {
+      data.response = JSON.parse(input)
+    } catch (e) {
+      return next(e)
+    }
+    next()
+  })
+}
+
+function serveDynamic(api, data, next) {
+  var form = new FormData()
+  form.append('api_token', api.config.poeditor.api_token)
+  form.append('id', api.config.poeditor.project_id)
+  form.append('language', data.params.lang)
+  fetch('https://api.poeditor.com/v2/terms/list', { method: 'POST', body: form })
+    .then(function (res) { return res.json() })
+    .then(function (json) {
+      var terms = json && json.result && json.result.terms || []
+      terms.map(function (item) {
+        var term = item.term
+        var translation = item.translation && item.translation.content
+        if (translation) {
+          data.response[ term ] = translation
+        }
+      })
+    })
+    .then(function () { next() }, next)
+}
 
 exports.i18n = {
   name: 'i18n',
   description: 'i18n',
-  inputs: {lang: {required: true}},
+  inputs: { lang: { required: true } },
   run: function (api, data, next) {
-    var form = new FormData()
-    form.append('api_token', '1b93cc52b4a133c6eeca1bba0ec219fe')
-    form.append('id', '146255')
-    form.append('language', data.params.lang)
-    fetch('https://api.poeditor.com/v2/terms/list', {method: 'POST', body: form})
-      .then(function (res) {
-        return res.json()
-      })
-      .then(function (json) {
-        var terms = json && json.result && json.result.terms
-        if (terms) {
-          terms.map(function (item) {
-            var term = item.term
-            var translation = item.translation && item.translation.content
-            if (translation) {
-              data.response[term] = translation
-            }
-          })
-        }
-      })
-      .then(function () {
-        next()
-      })
-      .catch(function (error) {
-        next(error)
-      })
+    if (!api.config.poeditor.enabled) {
+      return serveStatic(api, data, next)
+    } else {
+      return serveDynamic(api, data, next)
+    }
   }
 }
