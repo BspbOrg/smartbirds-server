e9937579caf8e040b9c70e721bee650852d2359a
Geno Roupsky <geno@roupsky.name>
2017-12-10 09:30:19 +0200

    Fix crash when no mailchimp configuration is provided

--- server/initializers/mailchimp.js
+++ server/initializers/mailchimp.js
@@ -2,6 +2,10 @@ var Mailchimp = require('mailchimp-api-v3')
 
 module.exports = {
   initialize: function (api, next) {
+    if (!api.config.mailchimp.enabled) {
+      return next()
+    }
+
     var client = new Mailchimp(api.config.mailchimp.api_key)
 
     api.mailchimp = {
