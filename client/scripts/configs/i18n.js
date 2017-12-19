c689867f441cfd6145fd68bbbc10adcd4b53aa2d
Geno Roupsky <geno@roupsky.name>
2017-12-19 11:31:34 +0200

    Use new logo of Bspb and localize

--- client/scripts/configs/i18n.js
+++ client/scripts/configs/i18n.js
@@ -1,11 +1,18 @@
-require('../app').config(/* @ngInject */function ($translateProvider, ENDPOINT_URL) {
-  $translateProvider
-    .useUrlLoader(ENDPOINT_URL + '/i18n')
-    .registerAvailableLanguageKeys(['en', 'bg'], {
-      'en_*': 'en',
-      'bg_*': 'bg',
-      '*': 'en'
+require('../app')
+  .config(/* @ngInject */function ($translateProvider, ENDPOINT_URL) {
+    $translateProvider
+      .useUrlLoader(ENDPOINT_URL + '/i18n')
+      .registerAvailableLanguageKeys([ 'en', 'bg' ], {
+        'en_*': 'en',
+        'bg_*': 'bg',
+        '*': 'en'
+      })
+      .determinePreferredLanguage()
+      .preferredLanguage('bg')
+  })
+  .run(/* @ngInject */function ($translate, $rootScope) {
+    $rootScope.$language = $translate.proposedLanguage()
+    $rootScope.$on('$translateChangeSuccess', function (e, params) {
+      $rootScope.$language = params.language
     })
-    .determinePreferredLanguage()
-    .preferredLanguage('bg')
-})
+  })
