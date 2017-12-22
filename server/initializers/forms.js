1e6b8539cb4f07fc0d6aaa76f102c6253d931264
Geno Roupsky <geno@roupsky.name>
2017-12-22 19:02:58 +0200

    Simplified forms initializer

--- server/initializers/forms.js
+++ server/initializers/forms.js
@@ -2,12 +2,7 @@ module.exports = {
   initialize: function (api, next) {
     api.forms = {
       isModerator: function (user, modelName) {
-        if (user.role === 'moderator') {
-          if (user.forms && user.forms[modelName]) {
-            return true
-          }
-        }
-        return false
+        return user.role === 'moderator' && user.forms && user.forms[ modelName ]
       }
     }
     next()
