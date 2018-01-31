7af6a3a8671bc057407fc7d09464f567cd1814be
Daniel Petrov <dani@code6.ninja>
2018-01-31 11:29:29 +0200

    set max export count in the controller

--- client/scripts/controllers/MonitoringController.js
+++ client/scripts/controllers/MonitoringController.js
@@ -7,6 +7,7 @@ var angular = require('angular')
 require('../app').controller('MonitoringController', /* @ngInject */function ($state, $stateParams, $q, $translate, model, ngToast, db, Raven, ENDPOINT_URL, $httpParamSerializer, formName) {
   var controller = this
 
+  controller.maxExportCount = 20000
   controller.formName = formName
   controller.db = db
   controller.filter = angular.copy($stateParams)
