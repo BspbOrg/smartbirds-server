5ae4666fe269ed6236415154f8a1c27fa1f4428c
Daniel Petrov <dani@code6.ninja>
2018-02-01 15:30:33 +0200

    add shares relation and create migration

---
+++ server/migrations/20180201131052-create-shares.js
@@ -0,0 +1,33 @@
+'use strict'
+
+var Sequelize = require('sequelize')
+
+var tableName = 'shares'
+
+var schema = {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  sharer: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  sharee: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+}
+
+module.exports = {
+  up: async function (queryInterface, Sequelize) {
+    if (queryInterface.sequelize.options.dialect !== 'postgres') return
+    await queryInterface.createTable(tableName, schema)
+  },
+
+  down: async function (queryInterface, Sequelize) {
+    if (queryInterface.sequelize.options.dialect !== 'postgres') return
+    await queryInterface.dropTable(tableName)
+  }
+}
