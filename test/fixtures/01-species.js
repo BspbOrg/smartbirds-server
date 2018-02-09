8ecf8de8350b38c5afe660e32e981f69f958bcbb
Geno Roupsky <geno@roupsky.name>
2018-02-09 21:52:35 +0200

    Fix tests

--- test/fixtures/01-species.js
+++ test/fixtures/01-species.js
@@ -38,5 +38,96 @@ module.exports = [
       labelBg: 'Индийско шаварче',
       labelEn: 'Paddyfield Warbler'
     }
+  },
+  {
+    model: 'species',
+    data: {
+      id: 5,
+      type: 'invertebrates',
+      labelLa: 'Lucanus cervus'
+    }
+  },
+  {
+    model: 'species',
+    data: {
+      id: 6,
+      type: 'mammals',
+      labelLa: 'Accipiter nisus'
+    }
+  },
+  {
+    model: 'species',
+    data: {
+      id: 7,
+      type: 'mammals',
+      labelLa: 'Aix sponsa'
+    }
+  },
+  {
+    model: 'species',
+    data: {
+      id: 8,
+      type: 'mammals',
+      labelLa: 'Acrocephalus agricola'
+    }
+  },
+  {
+    model: 'species',
+    data: {
+      id: 9,
+      type: 'herptiles',
+      labelLa: 'Accipiter nisus'
+    }
+  },
+  {
+    model: 'species',
+    data: {
+      id: 10,
+      type: 'herptiles',
+      labelLa: 'Aix sponsa'
+    }
+  },
+  {
+    model: 'species',
+    data: {
+      id: 11,
+      type: 'herptiles',
+      labelLa: 'Acrocephalus agricola'
+    }
+  },
+  {
+    model: 'species',
+    data: {
+      type: 'herp',
+      labelLa: 'Accipiter nisus'
+    }
+  },
+  {
+    model: 'species',
+    data: {
+      type: 'herp',
+      labelLa: 'Aix sponsa'
+    }
+  },
+  {
+    model: 'species',
+    data: {
+      type: 'herp',
+      labelLa: 'Acrocephalus agricola'
+    }
+  },
+  {
+    model: 'species',
+    data: {
+      type: 'birds',
+      labelLa: 'Duplicated'
+    }
+  },
+  {
+    model: 'species',
+    data: {
+      type: 'herptiles',
+      labelLa: 'Duplicated'
+    }
   }
 ]
