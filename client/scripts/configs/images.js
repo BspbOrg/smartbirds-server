0fdbb229410d69a4bf6aa67efe511bc58a45f394
Geno Roupsky <geno@roupsky.name>
2018-06-11 17:27:16 +0300

    Increase max image attachment to 32mb

--- client/scripts/configs/images.js
+++ client/scripts/configs/images.js
@@ -1,10 +1,3 @@
 var app = require('../app')
 
-switch (process.env.NODE_ENV || 'development') {
-  case 'development':
-    app.constant('MAX_IMAGE_SIZE', 512 * 1024)
-    break
-  default:
-    app.constant('MAX_IMAGE_SIZE', 2 * 1024 * 1024)
-    break
-}
+app.constant('MAX_IMAGE_SIZE', 32000000) // 32mb
