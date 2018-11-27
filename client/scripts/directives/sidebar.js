15a6a74761c4e13f479c11b8a218744057553dc7
Daniel Petrov <dani@code6.ninja>
2018-11-27 11:42:54 +0200

    udpate sidebar groups state when ui state is changed

--- client/scripts/directives/sidebar.js
+++ client/scripts/directives/sidebar.js
@@ -2,10 +2,17 @@ require('../app').directive('sidebar', /* @ngInject */function () {
   return {
     templateUrl: '/views/directives/sidebar.html',
     scope: {},
-    controller: /* @ngInject */function ($scope, $q, $state, api) {
-      this.isPrivateCollapsed = !$state.includes('auth.monitoring.private')
-      this.isPublicCollapsed = !$state.includes('auth.monitoring.public')
-      this.isStatsCollapsed = !$state.includes('auth.stats')
+    controller: /* @ngInject */function ($scope, $rootScope, $q, $state, api) {
+      var ctrl = this
+      ctrl.isPrivateCollapsed = !$state.includes('auth.monitoring.private')
+      ctrl.isPublicCollapsed = !$state.includes('auth.monitoring.public')
+      ctrl.isStatsCollapsed = !$state.includes('auth.stats')
+
+      $rootScope.$on('$stateChangeSuccess', function () {
+        ctrl.isPrivateCollapsed = !$state.includes('auth.monitoring.private')
+        ctrl.isPublicCollapsed = !$state.includes('auth.monitoring.public')
+        ctrl.isStatsCollapsed = !$state.includes('auth.stats')
+      })
     },
     controllerAs: '$ctrl'
   }
