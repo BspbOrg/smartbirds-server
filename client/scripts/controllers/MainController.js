4131ea5836300493412ed139cbb60a4db231f07b
Geno Roupsky <geno@roupsky.name>
2019-05-04 13:02:43 +0300

    Display local records in menu

--- client/scripts/controllers/MainController.js
+++ client/scripts/controllers/MainController.js
@@ -1,10 +1,10 @@
-/**
- * Created by groupsky on 10.12.15.
- */
+var forms = require('../configs/forms')
 
-require('../app').controller('MainController', /* @ngInject */function () {
+require('../app').controller('MainController', /* @ngInject */function ($injector) {
   var ctrl = this
   ctrl.$onInit = function () {
     ctrl.SW = window.SW_STATUS
   }
+
+  ctrl.forms = forms
 })
