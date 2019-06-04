6a0f03c7a6a3d4881fa6f1ad4a74347fa6b00cbe
Daniel Petrov <dani@code6.ninja>
2019-06-04 13:33:12 +0300

    Create threats form model definition and migration

---
+++ server/forms/threats.js
@@ -0,0 +1,83 @@
+const _ = require('lodash')
+const { assign } = Object
+
+exports = module.exports = _.cloneDeep(require('./_common'))
+
+exports.tableName = 'FormThreats'
+exports.hasSpecies = true
+
+exports.fields = assign(exports.fields, {
+  category: {
+    type: 'choice',
+    public: true,
+    required: true,
+    uniqueHash: true,
+    relation: {
+      model: 'nomenclature',
+      filter: { type: 'threats_category' }
+    }
+  },
+  species: {
+    type: 'choice',
+    public: true,
+    uniqueHash: true,
+    relation: {
+      model: 'species'
+    }
+  },
+  estimate: {
+    type: 'choice',
+    relation: {
+      model: 'nomenclature',
+      filter: { type: 'threats_estimate' }
+    }
+  },
+  poisonedType: 'text',
+  stateCarcass: {
+    type: 'choice',
+    relation: {
+      model: 'nomenclature',
+      filter: { type: 'threats_state_carcass' }
+    }
+  },
+  sampleTaken1: {
+    type: 'choice',
+    relation: {
+      model: 'nomenclature',
+      filter: { type: 'threats_sample_taken' }
+    }
+  },
+  sampleTaken2: {
+    type: 'choice',
+    relation: {
+      model: 'nomenclature',
+      filter: { type: 'threats_sample_taken' }
+    }
+  },
+  sampleTaken3: {
+    type: 'choice',
+    relation: {
+      model: 'nomenclature',
+      filter: { type: 'threats_sample_taken' }
+    }
+  },
+  location: {
+    type: 'text',
+    required: true
+  },
+  class: 'text',
+  sampleCode1: 'text',
+  sampleCode2: 'text',
+  sampleCode3: 'text',
+  count: '+int',
+  threatsNotes: 'text'
+})
+
+exports.foreignKeys.push({
+  targetModelName: 'species',
+  as: 'speciesInfo',
+  foreignKey: 'species',
+  targetKey: 'labelLa'
+})
+
+exports.indexes.push({ fields: ['species'] })
---
+++ server/migrations/20190604101604-create-form-threats.js
@@ -0,0 +1,153 @@
+'use strict'
+
+var Sequelize = require('sequelize')
+
+var tableName = 'FormThreats'
+
+var schema = {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  categoryBg: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  categoryEn: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  species: Sequelize.TEXT,
+  estimateBg: Sequelize.TEXT,
+  estimateEn: Sequelize.TEXT,
+  poisonedType: Sequelize.TEXT,
+  stateCarcassBg: Sequelize.TEXT,
+  stateCarcassEn: Sequelize.TEXT,
+  sampleTaken1Bg: Sequelize.TEXT,
+  sampleTaken1Eg: Sequelize.TEXT,
+  sampleTaken2Bg: Sequelize.TEXT,
+  sampleTaken2En: Sequelize.TEXT,
+  sampleTaken3Bg: Sequelize.TEXT,
+  sampleTaken3En: Sequelize.TEXT,
+  sampleCode1Bg: Sequelize.TEXT,
+  sampleCode1En: Sequelize.TEXT,
+  sampleCode2Bg: Sequelize.TEXT,
+  sampleCode2En: Sequelize.TEXT,
+  sampleCode3Bg: Sequelize.TEXT,
+  sampleCode3En: Sequelize.TEXT,
+  class: Sequelize.TEXT,
+  count: Sequelize.INTEGER,
+  threatsNotes: Sequelize.TEXT,
+  location: Sequelize.TEXT,
+
+  // Common fields not defined as common in Model.js
+  latitude: {
+    type: Sequelize.FLOAT,
+    allowNull: false
+  },
+  longitude: {
+    type: Sequelize.FLOAT,
+    allowNull: false
+  },
+  observationDateTime: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  monitoringCode: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  // CommonFields as defined in model!!
+  endDateTime: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  startDateTime: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  observers: Sequelize.TEXT,
+  rainBg: Sequelize.TEXT,
+  rainEn: Sequelize.TEXT,
+  temperature: Sequelize.FLOAT,
+  windDirectionBg: Sequelize.TEXT,
+  windDirectionEn: Sequelize.TEXT,
+  windSpeedBg: Sequelize.TEXT,
+  windSpeedEn: Sequelize.TEXT,
+  cloudinessBg: Sequelize.TEXT,
+  cloudinessEn: Sequelize.TEXT,
+  cloudsType: Sequelize.TEXT,
+  visibility: Sequelize.FLOAT,
+  mto: Sequelize.TEXT,
+  notes: Sequelize.TEXT,
+  threatsBg: Sequelize.TEXT,
+  threatsEn: Sequelize.TEXT,
+
+  // Internal
+  userId: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  createdAt: {
+    allowNull: false,
+    type: Sequelize.DATE
+  },
+  updatedAt: {
+    allowNull: false,
+    type: Sequelize.DATE
+  },
+  imported: Sequelize.INTEGER,
+  pictures: Sequelize.BLOB,
+  track: Sequelize.TEXT,
+  hash: Sequelize.STRING(64),
+  confidential: Sequelize.BOOLEAN
+}
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface
+      .createTable(tableName, schema)
+      .then(function () {
+        return queryInterface.addIndex(tableName, {
+          fields: ['userId']
+        })
+      })
+      .then(function () {
+        return queryInterface.addIndex(tableName, {
+          fields: ['observationDateTime']
+        })
+      })
+      .then(function () {
+        return queryInterface.addIndex(tableName, {
+          fields: ['species']
+        })
+      })
+      .then(function () {
+        return queryInterface.addIndex(tableName, {
+          fields: ['monitoringCode']
+        })
+      })
+      .then(function () {
+        return queryInterface.addIndex(tableName, {
+          fields: ['location']
+        })
+      })
+      .then(function () {
+        return queryInterface.addIndex(tableName, {
+          unique: true,
+          fields: ['hash']
+        })
+      })
+      .catch(function (e) {
+        return module.exports.down(queryInterface, Sequelize)
+          .finally(function () {
+            return Promise.reject(e)
+          })
+      })
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.dropTable(tableName)
+  }
+}
