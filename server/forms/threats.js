c081e9adb87d9c863821cea7f97efb8ed6c38ca2
Daniel Petrov <dani@code6.ninja>
2019-06-24 18:22:05 +0300

    Add preSave hook to remove unused values

--- server/forms/threats.js
+++ server/forms/threats.js
@@ -1,5 +1,6 @@
 const _ = require('lodash')
 const { assign } = Object
+const formsConfig = require('../../config/formsConfig')
 
 exports = module.exports = _.cloneDeep(require('./_common'))
 
@@ -99,3 +100,47 @@ exports.filterList = async function (api, data, q) {
 }
 
 exports.indexes.push({ fields: ['species'] })
+
+const preSave = function (threat, options) {
+  if (threat.primaryType === formsConfig.threatsPrimaryTypes.threat.id) {
+    threat.poisonedType = null
+    threat.stateCarcassBg = null
+    threat.stateCarcassEn = null
+    threat.sampleCode1 = null
+    threat.sampleCode2 = null
+    threat.sampleCode3 = null
+    threat.sampleTaken1Bg = null
+    threat.sampleTaken1En = null
+    threat.sampleTaken2Bg = null
+    threat.sampleTaken2En = null
+    threat.sampleTaken3Bg = null
+    threat.sampleTaken3En = null
+  } else {
+    threat.categoryBg = null
+    threat.categoryEn = null
+    threat.estimateBg = null
+    threat.estimateEn = null
+    switch (threat.poisonedType) {
+      case formsConfig.threatsPoisonedType.alive.id:
+        threat.stateCarcassBg = null
+        threat.stateCarcassEn = null
+        break
+      case formsConfig.threatsPoisonedType.bait.id:
+        threat.stateCarcassBg = null
+        threat.stateCarcassEn = null
+        threat.class = null
+        threat.speciesBg = null
+        threat.speciesEn = null
+        break
+      default:
+        break
+    }
+  }
+}
+
+exports.hooks = {
+  beforeCreate: preSave,
+  beforeUpdate: preSave,
+  beforeSave: preSave,
+  beforeUpsert: preSave
+}
