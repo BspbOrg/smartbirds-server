c4a22b2cee02b5fab5708ece142c5f554ccf0212
Daniel Petrov <d.petrov86@gmail.com>
2019-07-10 09:52:56 +0300

    Create Threats form (#73)
    Create new form for threats

---
+++ server/migrations/20190604101604-create-form-threats.js
@@ -0,0 +1,152 @@
+'use strict'
+
+var Sequelize = require('sequelize')
+
+var tableName = 'FormThreats'
+
+var schema = {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  categoryBg: Sequelize.TEXT,
+  categoryEn: Sequelize.TEXT,
+  species: Sequelize.TEXT,
+  estimateBg: Sequelize.TEXT,
+  estimateEn: Sequelize.TEXT,
+  poisonedType: Sequelize.TEXT,
+  stateCarcassBg: Sequelize.TEXT,
+  stateCarcassEn: Sequelize.TEXT,
+  sampleTaken1Bg: Sequelize.TEXT,
+  sampleTaken1En: Sequelize.TEXT,
+  sampleTaken2Bg: Sequelize.TEXT,
+  sampleTaken2En: Sequelize.TEXT,
+  sampleTaken3Bg: Sequelize.TEXT,
+  sampleTaken3En: Sequelize.TEXT,
+  sampleCode1: Sequelize.TEXT,
+  sampleCode2: Sequelize.TEXT,
+  sampleCode3: Sequelize.TEXT,
+  class: Sequelize.TEXT,
+  count: Sequelize.INTEGER,
+  threatsNotes: Sequelize.TEXT,
+  primaryType: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  location: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+
+  // Common fields not defined as common in Model.js
+  latitude: {
+    type: Sequelize.FLOAT,
+    allowNull: false
+  },
+  longitude: {
+    type: Sequelize.FLOAT,
+    allowNull: false
+  },
+  observationDateTime: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  monitoringCode: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  // CommonFields as defined in model!!
+  endDateTime: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  startDateTime: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  observers: Sequelize.TEXT,
+  rainBg: Sequelize.TEXT,
+  rainEn: Sequelize.TEXT,
+  temperature: Sequelize.FLOAT,
+  windDirectionBg: Sequelize.TEXT,
+  windDirectionEn: Sequelize.TEXT,
+  windSpeedBg: Sequelize.TEXT,
+  windSpeedEn: Sequelize.TEXT,
+  cloudinessBg: Sequelize.TEXT,
+  cloudinessEn: Sequelize.TEXT,
+  cloudsType: Sequelize.TEXT,
+  visibility: Sequelize.FLOAT,
+  mto: Sequelize.TEXT,
+  notes: Sequelize.TEXT,
+  threatsBg: Sequelize.TEXT,
+  threatsEn: Sequelize.TEXT,
+
+  // Internal
+  userId: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  createdAt: {
+    allowNull: false,
+    type: Sequelize.DATE
+  },
+  updatedAt: {
+    allowNull: false,
+    type: Sequelize.DATE
+  },
+  imported: Sequelize.INTEGER,
+  pictures: Sequelize.BLOB,
+  track: Sequelize.TEXT,
+  hash: Sequelize.STRING(64),
+  confidential: Sequelize.BOOLEAN,
+  geolocationAccuracy: Sequelize.FLOAT
+}
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface
+      .createTable(tableName, schema)
+      .then(function () {
+        return queryInterface.addIndex(tableName, {
+          fields: ['userId']
+        })
+      })
+      .then(function () {
+        return queryInterface.addIndex(tableName, {
+          fields: ['observationDateTime']
+        })
+      })
+      .then(function () {
+        return queryInterface.addIndex(tableName, {
+          fields: ['species']
+        })
+      })
+      .then(function () {
+        return queryInterface.addIndex(tableName, {
+          fields: ['monitoringCode']
+        })
+      })
+      .then(function () {
+        return queryInterface.addIndex(tableName, {
+          fields: ['location']
+        })
+      })
+      .then(function () {
+        return queryInterface.addIndex(tableName, {
+          unique: true,
+          fields: ['hash']
+        })
+      })
+      .catch(function (e) {
+        return module.exports.down(queryInterface, Sequelize)
+          .finally(function () {
+            return Promise.reject(e)
+          })
+      })
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.dropTable(tableName)
+  }
+}
---
+++ server/migrations/20190604111254-insert-form-threats-nomenclatures.js
@@ -0,0 +1,110 @@
+'use strict'
+
+var nomenclatures = [
+  {
+    name: 'threats_category',
+    values: [
+      { bg: 'Пожари', en: 'Fires' },
+      { bg: 'Сечи', en: 'Logging' },
+      { bg: 'Строежи', en: 'Construction / development' },
+      { bg: 'Замърсяване', en: 'Pollution' },
+      { bg: 'Кариери', en: 'Quarries' },
+      { bg: 'Разораване на пасища и мери', en: 'Plowing of pastures' },
+      { bg: 'Зарибяване', en: 'Stocking with fish' },
+      { bg: 'Бракониери', en: 'Poachers' },
+      { bg: 'Залесяване с нетипични видове', en: 'Planting with non-typical species' },
+      { bg: 'Пътища', en: 'Roads' },
+      { bg: 'Мъртъв екземпляр', en: 'Dead individual' },
+      { bg: 'Ветропарк', en: 'Wind park' },
+      { bg: 'Прекомерно обрастване на пасища', en: 'Excessive overgrowth of pastures' },
+      { bg: 'Други', en: 'Other' },
+      { bg: 'Соларен парк', en: 'Solar park' },
+      { bg: 'Инвазивни видове', en: 'Invasive species' },
+      { bg: 'Премахване на храсти', en: 'Removal of shrubs' },
+      { bg: 'Мини ВЕЦ', en: 'Small hydroelectric' },
+      { bg: 'Пресъхващи водни тела', en: 'Dried up waterbodies' },
+      { bg: 'Убит на пътя', en: 'Dead on road' },
+      { bg: 'Ловна преса', en: 'Hunting' },
+      { bg: 'Тровене', en: 'Poisoning' },
+      { bg: 'Безпокойство', en: 'Disturbance' },
+      { bg: 'Иманярство', en: 'Treasure hunting' },
+      { bg: 'Екстремни спортове', en: 'Extreme sports' },
+      { bg: 'Туристическа дейност', en: 'Touristic activities' }
+    ]
+  },
+  {
+    name: 'threats_estimate',
+    values: [
+      { bg: 'Ниско', en: 'Low' },
+      { bg: 'Средно', en: 'Average' },
+      { bg: 'Високо', en: 'High' }
+    ]
+  },
+  {
+    name: 'threats_state_carcass',
+    values: [
+      { bg: 'Сежо/ново', en: 'Fresh' },
+      { bg: 'Подуто', en: 'Bloat' },
+      { bg: 'Активно разлагане', en: 'Active decay' },
+      { bg: 'Напреднало разлагане', en: 'Advanced decay' },
+      { bg: 'Сухо/останки', en: 'Dry/remains' },
+      { bg: 'Неизвестно/няма', en: 'Unknown/Not available' }
+    ]
+  },
+  {
+    name: 'threats_sample',
+    values: [
+      { bg: 'Цял труп', en: 'Whole body' },
+      { bg: 'Стомах', en: 'Stomach' },
+      { bg: 'Гуша', en: 'Crop' },
+      { bg: 'Черен дроб', en: 'Liver' },
+      { bg: 'Мозък', en: 'Brain' },
+      { bg: 'Нокти', en: 'Talons/ Claws' },
+      { bg: 'Други', en: 'Other' },
+      { bg: 'Не', en: 'None' },
+      { bg: 'Няма', en: 'Unknown/Not available' },
+      { bg: 'Бъбреци', en: 'Kidney' },
+      { bg: 'Далак', en: 'Spleen' },
+      { bg: 'Кости', en: 'Bones' },
+      { bg: 'Жлъчка', en: 'Gall' },
+      { bg: 'Бял дроб', en: 'Lungs' },
+      { bg: 'Сърце', en: 'Heart' },
+      { bg: 'Пера', en: 'Feathers' }
+    ]
+  }
+]
+
+module.exports = {
+  up: async function (queryInterface, Sequelize) {
+    if (queryInterface.sequelize.options.dialect !== 'postgres') return
+
+    var nomenclatureValues = nomenclatures
+      .map(function (nomenclature) {
+        return nomenclature.values
+          .map(function (value) {
+            return {
+              type: nomenclature.name,
+              labelBg: value.bg,
+              labelEn: value.en,
+              createdAt: new Date(),
+              updatedAt: new Date()
+            }
+          })
+      })
+      .reduce(function (res, current) {
+        return res.concat(current)
+      })
+
+    await queryInterface.bulkInsert('Nomenclatures', nomenclatureValues)
+  },
+
+  down: async function (queryInterface, Sequelize) {
+    if (queryInterface.sequelize.options.dialect !== 'postgres') return
+
+    await queryInterface.bulkDelete('Nomenclatures', {
+      type: {
+        $in: nomenclatures.map(function (nomenclature) { return nomenclature.name })
+      }
+    })
+  }
+}
