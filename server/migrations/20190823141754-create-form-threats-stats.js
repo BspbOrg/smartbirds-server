cb8e76c7a2c739d87a540eae6af9086bd85e8cb8
Daniel Petrov <dani@code6.ninja>
2019-08-23 18:04:09 +0300

    Create threats public page with map

--- server/migrations/20190823141754-create-form-threats-stats.js
+++ server/migrations/20190823141754-create-form-threats-stats.js
@@ -18,3 +18,40 @@ module.exports = {
     return queryInterface.sequelize.query('DROP VIEW IF EXISTS threats_stats')
   }
 }
+
+// SELECT lat,
+//        lon,
+//        threatsBg,
+//        threatsEn,
+//        form
+// FROM (
+//         (SELECT latitude AS lat,
+//                 longitude AS lon,
+//                 unnest(string_to_array("threatsBg", ' | ')) AS threatsBg,
+//                 unnest(string_to_array("threatsEn", ' | ')) AS threatsEn,
+//                 'cbm' AS form
+//          FROM "FormCBM"
+//          WHERE "threatsBg" IS NOT NULL
+//            AND "threatsBg" != ''
+//            AND id = 175019 )
+//       UNION ALL
+//         (SELECT latitude AS lat,
+//                 longitude AS lon,
+//                 unnest(string_to_array("threatsBg", ' | ')) AS threatsBg,
+//                 unnest(string_to_array("threatsEn", ' | ')) AS threatsEn,
+//                 'birds' AS form
+//          FROM "FormBirds"
+//          WHERE "threatsBg" IS NOT NULL
+//            AND "threatsBg" != ''
+//            AND id = 169734 )
+//       UNION ALL
+// (SELECT latitude AS lat,
+//     longitude AS lon,
+//     CASE WHEN "primaryType" = 'threat' THEN "categoryBg"
+//     ELSE 'Тровене'
+//     END AS threatBg,
+//     CASE WHEN "primaryType" = 'threat' THEN "categoryEn"
+//     ELSE 'Poisoning'
+//     END AS threatEn,
+// 'threats' AS form
+// FROM "FormThreats" )) AS all_forms
