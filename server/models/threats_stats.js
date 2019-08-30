5cf7b9716f91830d447ca6ead5b817216c537bc7
Geno Roupsky <geno@roupsky.name>
2019-08-30 15:14:06 +0300

    Update threats stats model

--- server/models/threats_stats.js
+++ server/models/threats_stats.js
@@ -3,7 +3,9 @@ module.exports = function (sequelize, DataTypes) {
   return sequelize.define('threats_stats', {
     latitude: { type: DataTypes.DOUBLE, primaryKey: true },
     longitude: { type: DataTypes.DOUBLE, primaryKey: true },
-    threats_count: DataTypes.INTEGER
+    threatsBg: DataTypes.TEXT,
+    threatsEn: DataTypes.TEXT,
+    form: DataTypes.TEXT
   }, {
     timestamps: false,
     tableName: 'threats_stats'
