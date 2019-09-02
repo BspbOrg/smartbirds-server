837098289f79cc65e5dbcdbfec83ee40f61cfce9
Geno Roupsky <geno@roupsky.name>
2019-09-02 11:08:59 +0300

    Directive to display threat type

---
+++ client/scripts/directives/threatType.js
@@ -0,0 +1,17 @@
+require('../app')
+/**
+ * A helper directive to display a threat record primary type and subtype
+ */
+  .directive('threatType', /* @ngInject */function () {
+    return {
+      restrict: 'E',
+      template: [
+        '<config-label config="threatsPrimaryTypes" value="{{::record.primaryType}}"></config-label>',
+        '<span ng-if="::record.category">{{::record.category.label[$root.$language]}}</span>',
+        '<config-label ng-if="::record.poisonedType" config="threatsPoisonedType" value="{{::record.poisonedType}}"></config-label>'
+      ].join(' '),
+      scope: {
+        record: '<'
+      }
+    }
+  })
