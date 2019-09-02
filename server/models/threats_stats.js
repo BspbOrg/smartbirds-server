d92b8cb86a56b3cae8c35d83b5cc7d53f1a9aaab
Daniel Petrov <dani@code6.ninja>
2019-09-02 13:30:30 +0300

    Public threats (#76)
    * Create threats stats model and add in statistics task
    * Create threats public page with map
    * Update threats stats model
    * Display category on threats map tooltip

---
+++ server/models/threats_stats.js
@@ -0,0 +1,13 @@
+'use strict'
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define('threats_stats', {
+    latitude: { type: DataTypes.DOUBLE, primaryKey: true },
+    longitude: { type: DataTypes.DOUBLE, primaryKey: true },
+    threatsBg: DataTypes.TEXT,
+    threatsEn: DataTypes.TEXT,
+    form: DataTypes.TEXT
+  }, {
+    timestamps: false,
+    tableName: 'threats_stats'
+  })
+}
