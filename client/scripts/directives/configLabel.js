07df29ee1db4f1516f93bc889017a4c3b39a89f6
Geno Roupsky <geno@roupsky.name>
2019-09-02 11:18:45 +0300

    Handle empty value for configLabel and invalid config

--- client/scripts/directives/configLabel.js
+++ client/scripts/directives/configLabel.js
@@ -17,7 +17,20 @@ require('../app').directive('configLabel', /* @ngInject */function () {
         throw new Error('Unsupported config type: "' + $attrs.config + '"\nAvailable values are: ' + Object.keys(formsConfig).join(', '))
       }
 
-      $translate(formsConfig[$attrs.config][$scope.value].label).then(function (val) { $ctrl.label = val }).catch(angular.noop)
+      // if not selected, display nothing
+      if (!$scope.value) {
+        $ctrl.label = ''
+        return
+      }
+
+      var currentValue = formsConfig[$attrs.config][$scope.value]
+
+      // validate config has label
+      if (!('label' in currentValue)) {
+        throw new Error('formsConfig[' + $attrs.config + '][' + $scope.value + '] is missing label key')
+      }
+
+      $translate(currentValue.label).then(function (val) { $ctrl.label = val }).catch(angular.noop)
       $ctrl.label = $translate.instant(formsConfig[$attrs.config][$scope.value].label)
     },
     controllerAs: '$ctrl'
