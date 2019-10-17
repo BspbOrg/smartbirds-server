dd50d150b656c4eee1c26083a9cb2d108c5b7bb2
Daniel Petrov <dani@code6.ninja>
2019-10-17 17:21:03 +0300

    Add operator aliases

--- server/initializers/sequelize.js
+++ server/initializers/sequelize.js
@@ -3,6 +3,43 @@ var fs = require('fs')
 var Sequelize = require('sequelize')
 var Umzug = require('umzug')
 var _ = require('lodash')
+const Op = Sequelize.Op
+const operatorsAliases = {
+  $eq: Op.eq,
+  $ne: Op.ne,
+  $gte: Op.gte,
+  $gt: Op.gt,
+  $lte: Op.lte,
+  $lt: Op.lt,
+  $not: Op.not,
+  $in: Op.in,
+  $notIn: Op.notIn,
+  $is: Op.is,
+  $like: Op.like,
+  $notLike: Op.notLike,
+  $iLike: Op.iLike,
+  $notILike: Op.notILike,
+  $regexp: Op.regexp,
+  $notRegexp: Op.notRegexp,
+  $iRegexp: Op.iRegexp,
+  $notIRegexp: Op.notIRegexp,
+  $between: Op.between,
+  $notBetween: Op.notBetween,
+  $overlap: Op.overlap,
+  $contains: Op.contains,
+  $contained: Op.contained,
+  $adjacent: Op.adjacent,
+  $strictLeft: Op.strictLeft,
+  $strictRight: Op.strictRight,
+  $noExtendRight: Op.noExtendRight,
+  $noExtendLeft: Op.noExtendLeft,
+  $and: Op.and,
+  $or: Op.or,
+  $any: Op.any,
+  $all: Op.all,
+  $values: Op.values,
+  $col: Op.col
+}
 
 module.exports = {
   loadPriority: 310,
@@ -15,6 +52,7 @@ module.exports = {
       api.config.sequelize.password,
       {
         ...api.config.sequelize,
+        operatorsAliases,
         hooks: {
           afterDefine (model) {
             if (model.options.classMethods) {
