50dfc085d6dbde8382ce2e7083145bde74727df6
Daniel Petrov <dani@code6.ninja>
2019-10-17 10:12:08 +0300

    fixup! Use runHooks as instance method of the model

--- server/initializers/forms.js
+++ server/initializers/forms.js
@@ -285,7 +285,7 @@ function generateExportData (form) {
 
 function generateApiUpdate (fields) {
   return async function (data) {
-    await this.runHooks('beforeApiUpdate', this, data)
+    await this.constructor.runHooks('beforeApiUpdate', this, data)
     _.forEach(fields, (field, name) => {
       if (_.isString(field)) field = { type: field }
       switch (field.type) {
@@ -364,7 +364,7 @@ function generateApiUpdate (fields) {
           break
       }
     })
-    await this.runHooks('afterApiUpdate', this, data)
+    await this.constructor.runHooks('afterApiUpdate', this, data)
     return this
   }
 }
