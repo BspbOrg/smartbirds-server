b8fd37a2aacf3d4293cb3a2cc1fb8fdd452c7e98
Daniel Petrov <dani@code6.ninja>
2019-10-18 17:08:20 +0300

    Use mocka config instead opt file

---
+++ .mocharc.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = {
+  colors: true,
+  recursive: true,
+  reporter: 'spec',
+  require: ['should'],
+  globals: ['config'],
+  timeout: 5000,
+  slow: 250,
+  opts: false
+}
