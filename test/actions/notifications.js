f102a6781718a3a649830ed07f33565946168298
Daniel Petrov <dani@code6.ninja>
2019-11-01 15:34:29 +0200

    Add describe.skip instead of empty file othwerwise jest is failing.

--- test/actions/notifications.js
+++ test/actions/notifications.js
@@ -3,3 +3,8 @@
 // TODO admin should have notification about requested ownership
 // TODO user should have notification about accepted ownership
 // TODO user should have notification about rejected ownership
+describe.skip('Notifications', () => {
+  it('admin should have notification about requested ownership')
+  it('user should have notification about accepted ownership')
+  it('user should have notification about rejected ownership')
+})
