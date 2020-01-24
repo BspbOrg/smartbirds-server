2b20560f65625d87304735515f7592608248da48
Daniel Petrov <dani@code6.ninja>
2020-01-24 19:15:14 +0200

    Create task for exporting organizations list as public json

---
+++ server/tasks/exportOrganizations.js
@@ -0,0 +1,21 @@
+const { Task, api } = require('actionhero')
+var Promise = require('bluebird')
+var writeFile = Promise.promisify(require('fs').writeFile)
+
+module.exports = class ExportOrganizationsTask extends Task {
+  constructor () {
+    super()
+    this.name = 'organizations:export'
+    this.description = 'Export organizations in public json accessed withoud authentication'
+    this.frequency = 0
+    this.queue = 'default'
+  }
+
+  async run (data) {
+    const organizations = (await api.models.organization.findAll({
+      order: [['id', 'ASC']]
+    })).map(organization => organization.apiData(api))
+
+    await writeFile(api.config.general.paths.public[0] + '/organizations.json', JSON.stringify(organizations))
+  }
+}
