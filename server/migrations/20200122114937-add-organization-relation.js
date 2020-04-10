19562c8131fd4560d082dac745ca9e65dd12509e
Geno Roupsky <geno@roupsky.name>
2020-04-10 14:42:52 +0300

    Fix migration on tests (#333)
    A migration tries to drop default on a column, but that fails in sqlite. In this particular case it's safe to skip it.

--- server/migrations/20200122114937-add-organization-relation.js
+++ server/migrations/20200122114937-add-organization-relation.js
@@ -3,7 +3,11 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Users', 'organizationSlug', { type: Sequelize.TEXT, defaultValue: 'bspb', allowNull: false })
-    await queryInterface.sequelize.query('ALTER TABLE "Users" ALTER COLUMN "organizationSlug" DROP DEFAULT;')
+
+    // sqlite cannot drop default, and it's for tests, so no harm
+    if (queryInterface.sequelize.options.dialect !== 'sqlite') {
+      await queryInterface.sequelize.query('ALTER TABLE "Users" ALTER COLUMN "organizationSlug" DROP DEFAULT;')
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
