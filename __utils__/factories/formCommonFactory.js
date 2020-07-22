77377e8f39388db10e1cce37abe3430d0be31c10
Geno Roupsky <geno@roupsky.name>
2020-07-22 21:03:54 +0300

    Suppress lgtm warning about insecure randomness in test code

--- __utils__/factories/formCommonFactory.js
+++ __utils__/factories/formCommonFactory.js
@@ -4,8 +4,8 @@ async function formCommonFactory (api, {
   user = 'user@smartbirds.com',
   userId = api.models.user.findOne({ where: { email: user } }),
   organization = 'bspb',
-  latitude = Math.random() * 180 - 90,
-  longitude = Math.random() * 360 - 180,
+  latitude = Math.random() * 180 - 90, // lgtm[js/insecure-randomness]
+  longitude = Math.random() * 360 - 180, // lgtm[js/insecure-randomness]
   observationDateTime = Date.now(),
   monitoringCode = `monitoring ${monitoringSequence++}`,
   startDateTime = observationDateTime,
