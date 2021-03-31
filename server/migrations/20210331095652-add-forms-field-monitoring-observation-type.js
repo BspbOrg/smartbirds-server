ce75e370bdc7099c9562e8a7e94af7a419594651
Geno Roupsky <geno@roupsky.name>
2021-03-31 14:01:52 +0300

    feat(nomenclatures): add nomenclature data

--- server/migrations/20210331095652-add-forms-field-monitoring-observation-type.js
+++ server/migrations/20210331095652-add-forms-field-monitoring-observation-type.js
@@ -9,6 +9,24 @@ const tables = [
   'FormThreats'
 ]
 
+const nomenclatures = [
+  {
+    name: 'main_observation_type',
+    values: [
+      { bg: 'Всички', en: 'Complete' },
+      { bg: 'Интересни', en: 'Interesting' }
+    ]
+  }
+]
+
+function deleteExistingValues (queryInterface, Sequelize) {
+  return queryInterface.bulkDelete('Nomenclatures', {
+    type: {
+      [Sequelize.Op.in]: nomenclatures.map(function (nomenclature) { return nomenclature.name })
+    }
+  })
+}
+
 module.exports = {
   up: async function (queryInterface, Sequelize) {
     await Promise.all(tables.map(async (table) => {
@@ -16,6 +34,30 @@ module.exports = {
       await queryInterface.addColumn(table, 'monitoringObservationTypeLocal', { type: Sequelize.TEXT })
       await queryInterface.addColumn(table, 'monitoringObservationTypeLang', { type: Sequelize.String(3) })
     }))
+
+    // we use hard-coded IDs for the nomenclatures in test fixtures and fixtures cannot be applied if the migration is executed
+    if (queryInterface.sequelize.options.dialect !== 'postgres') return
+
+    await deleteExistingValues(queryInterface, Sequelize)
+
+    const nomenclatureValues = nomenclatures
+      .map(function (nomenclature) {
+        return nomenclature.values
+          .map(function (value) {
+            return {
+              type: nomenclature.name,
+              labelBg: value.bg,
+              labelEn: value.en,
+              createdAt: new Date(),
+              updatedAt: new Date()
+            }
+          })
+      })
+      .reduce(function (res, current) {
+        return res.concat(current)
+      })
+
+    await queryInterface.bulkInsert('Nomenclatures', nomenclatureValues)
   },
 
   down: async function (queryInterface, Sequelize) {
@@ -24,5 +66,9 @@ module.exports = {
       await queryInterface.removeColumn(table, 'monitoringObservationTypeLocal')
       await queryInterface.removeColumn(table, 'monitoringObservationTypeLang')
     }))
+
+    if (queryInterface.sequelize.options.dialect !== 'postgres') return
+
+    await deleteExistingValues(queryInterface, Sequelize)
   }
 }
