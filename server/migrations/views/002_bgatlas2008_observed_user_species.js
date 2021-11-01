96e6f74faf748b165f7124fa832fc6abf6243b6c
Geno Roupsky <geno@roupsky.name>
2021-11-01 17:15:12 +0200

    feat(bgatlas): add records count to user stats
    Unfinished since the update of the materialized view required dropping all dependent views

--- server/migrations/views/002_bgatlas2008_observed_user_species.js
+++ server/migrations/views/002_bgatlas2008_observed_user_species.js
@@ -6,7 +6,8 @@ module.exports = {
       user_id,
       o.species,
       SUM(count)::integer as count,
-      (count(e.species) > 0) as existing
+      (count(e.species) > 0) as existing,
+      COUNT(*)::integer as records_count
     FROM birds_observations o
     LEFT JOIN bgatlas2008_species e ON (e.utm_code = o.bgatlas2008_utm_code AND e.species = o.species)
     WHERE bgatlas2008_utm_code IS NOT NULL
--- server/migrations/views/003_bgatlas2008_stats_user.js
+++ server/migrations/views/003_bgatlas2008_stats_user.js
@@ -6,14 +6,16 @@ module.exports = {
       c.user_id,
       COALESCE(spec_known, 0) as spec_known,
       COALESCE(spec_unknown, 0) as spec_unknown,
-      COALESCE((SELECT count(e.species)::integer FROM bgatlas2008_species e WHERE c.utm_code = e.utm_code), 0) as spec_old
+      COALESCE((SELECT count(e.species)::integer FROM bgatlas2008_species e WHERE c.utm_code = e.utm_code), 0) as spec_old,
+      records_count
     FROM (SELECT utm_code, u.id as user_id FROM bgatlas2008_cells c, "Users" u) c
     LEFT JOIN (
       SELECT
         utm_code,
         user_id,
-        sum(case when existing then 1 else 0 end)::integer as spec_known,
-        sum(case when existing then 0 else 1 end)::integer as spec_unknown
+        SUM(case when existing then 1 else 0 end)::integer AS spec_known,
+        SUM(case when existing then 0 else 1 end)::integer AS spec_unknown,
+        SUM(records_count)::integer as records_count
       FROM bgatlas2008_observed_user_species o
       GROUP BY utm_code, user_id
     ) s ON (c.utm_code = s.utm_code AND c.user_id = s.user_id)
