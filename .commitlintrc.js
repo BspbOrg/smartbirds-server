ec522aa5c96d9bc7085e9a7e91a2dd36ae132416
Geno Roupsky <geno@roupsky.name>
2022-09-25 19:52:00 +0300

    style(lint): relax commitlint length rules to warnings

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [1, 'always', 100],
+    'footer-max-line-length': [1, 'always', 100],
+    'header-max-length': [1, 'always', 100],
+  }
 }
