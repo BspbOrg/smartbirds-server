2b05480f6bd671d2d3e8e22047d12e0cf88bdec4
Geno Roupsky <geno@roupsky.name>
2022-09-28 15:51:34 +0300

    fix: birds migrations top species month count calculation

--- server/migrations/views/001_birds_migrations/002_birds_migrations_top_species_month.js
+++ server/migrations/views/001_birds_migrations/002_birds_migrations_top_species_month.js
@@ -1,11 +1,11 @@
 module.exports = {
   up: `
         CREATE OR REPLACE VIEW birds_migrations_top_species_month AS
-        SELECT species, COUNT(*) AS count
+        SELECT species, SUM(count) AS count
         FROM birds_migrations_observations form
         WHERE observation_date_time >= CURRENT_DATE - INTERVAL '1 month'
         GROUP BY species
-        ORDER BY COUNT(*) DESC
+        ORDER BY count DESC
   `,
   down: `
         DROP VIEW IF EXISTS birds_migrations_top_species_month
