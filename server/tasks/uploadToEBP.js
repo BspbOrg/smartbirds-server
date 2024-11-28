52a71e65bfb2b94a090a667f1a64e14db4845103
Daniel Petrov <dani@code6.ninja>
2024-11-28 16:09:40 +0200

    feat(ebp-upload): wip prepare data for upload

--- server/tasks/uploadToEBP.js
+++ server/tasks/uploadToEBP.js
@@ -1,4 +1,9 @@
-const { Task } = require('actionhero')
+const { Task, api } = require('actionhero')
+const sequelize = require('sequelize')
+const { Op } = sequelize
+
+// const availableForms = [api.forms.formCBM, api.forms.formBirds, api.forms.formCiconia]
+const availableForms = [api.forms.formBirds]
 
 // eslint-disable-next-line no-unused-vars
 const API_TOKEN = process.env.EBP_API_TOKEN
@@ -66,6 +71,55 @@ const apiParams = {
   }
 }
 
+const allowedOrganizations = () => {
+  return ['bspb', 'independent']
+}
+
+const excludedSources = () => {
+  return ['Project NMNH-BAS & MOEW', 'Research of breeding birds, BSPB-NMNH']
+}
+
+const getEbpSpecies = async () => {
+  return api.models.ebp_birds.findAll({
+    where: {
+      sbNameLa: { [Op.not]: null }
+    }
+  })
+}
+
+const loadRecords = async (forms, date) => {
+  const records = await Promise.allSettled(forms.map(async form => {
+    return form.model.findAll({
+      where: {
+        etrs89GridCode: { [Op.not]: null },
+        observationDateTime: {
+          [Op.and]: [
+            { [Op.gte]: '2024-01-13 00:00:00' },
+            { [Op.lte]: '2024-01-13 23:59:59' }
+          ]
+        },
+        organization: { [Op.in]: allowedOrganizations() },
+        sourceEn: { [Op.notIn]: excludedSources() }
+      }
+    })
+  }))
+
+  for (const record of records) {
+    if (record.status === 'rejected') {
+      throw record.reason
+    }
+  }
+
+  return records?.map(record => record.value).flat()
+}
+
+const prepareEbpData = async (date = new Date()) => {
+  const ebpSpecies = await getEbpSpecies()
+  console.log('+++++ EBP SPECIES: ', ebpSpecies?.length)
+  const records = await loadRecords(availableForms, date)
+  return records
+}
+
 module.exports = class UploadToEBP extends Task {
   constructor () {
     super()
@@ -77,7 +131,7 @@ module.exports = class UploadToEBP extends Task {
   }
 
   async run () {
-    // TODO: implement the task
-    console.log('Not implemented yet')
+    const ebpData = await prepareEbpData(new Date())
+    console.log('+++++ EBP DATA: ', ebpData?.length)
   }
 }
