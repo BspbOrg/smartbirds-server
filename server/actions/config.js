c1ee0abe1a03fc382e38a02bd5d8b046b4d2b712
Daniel Petrov <dani@code6.ninja>
2025-02-10 17:57:56 +0200

    fixup! feat(server-config): wip read server config

--- server/actions/config.js
+++ server/actions/config.js
@@ -1,5 +1,34 @@
 const { upgradeAction } = require('../utils/upgrade')
 
+const supportedVariables = [
+  'EBP_API_TOKEN',
+  'REDIS_HOST',
+  'REDIS_PORT',
+  'DATABASE_USER',
+  'DATABASE_PASSWORD',
+  'MAILGUN_API',
+  'MAILGUN_DOMAIN',
+  'MAILCHIMP_ENABLED',
+  'MAILCHIMP_API_KEY',
+  'MAILCHIMP_LIST_ID',
+  'SENTRY_DSN',
+  'POEDITOR_ENABLED',
+  'POEDITOR_API_TOKEN',
+  'ORPHAN_OWNER',
+  'SESSION_PREFIX',
+  'SESSION_DURATION',
+  'SERVER_TOKEN',
+  'BG_ATLAS_2008_MAX_RECORDS',
+  'AUTO_LOCATION_MAX_RECORDS',
+  'AUTO_VISIT_MAX_RECORDS',
+  'AUTO_TRANSLATE_MAX_RECORDS',
+  'TASKS_MAX_RECORDS',
+  'ETRS89_TASK_MAX_RECORDS',
+  'ETRS89_TASK_START_TIME',
+  'BG_ATLAS_2008_GRID_SIZE',
+  'BG_ATLAS_2008_START_TIMESTAMP'
+]
+
 exports.readServerConfig = upgradeAction('ah17', {
   name: 'server-config:get',
   description: 'server-config:get',
@@ -7,9 +36,44 @@ exports.readServerConfig = upgradeAction('ah17', {
 
   run: function (api, data, next) {
     try {
+      console.log('process.env', process.env)
       data.response.data = {
-        config: 'Here we will return the server config'
+        config: {
+          environmentVariables: supportedVariables.map(key => {
+            return {
+              key,
+              value: process.env[key] || ''
+            }
+          })
+        }
       }
+      return next()
+    } catch (e) {
+      console.error(e)
+      return next(e)
+    }
+  }
+})
+
+exports.updateServerConfig = upgradeAction('ah17', {
+  name: 'server-config:update',
+  description: 'server-config:update',
+  middleware: ['admin'],
+  inputs: {
+    key: {
+      required: true,
+      validator: (value) => {
+        return supportedVariables.includes(value)
+      }
+    },
+    value: {
+      required: true
+    }
+  },
+
+  run: function (api, data, next) {
+    try {
+      console.log('Updating config variable', data.params.key, 'to', data.params.value)
       return next()
     } catch (e) {
       console.error(e)
