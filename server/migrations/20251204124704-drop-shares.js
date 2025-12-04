56b1b2edbf934bc5e4a32d2cfa97f494f555df7d
Daniel Petrov <dani@code6.ninja>
2025-12-04 14:56:40 +0200

    feat(drop-friends): remove Share model

---
+++ server/migrations/20251204124704-drop-shares.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const Sequelize = require('sequelize')
+
+const tableName = 'Shares'
+
+const schema = {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  sharer: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  sharee: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+}
+
+module.exports = {
+  up: async function (queryInterface, Sequelize) {
+    await queryInterface.dropTable(tableName)
+  },
+
+  down: async function (queryInterface, Sequelize) {
+    await queryInterface.createTable(tableName, schema)
+  }
+}
