bc7fc72d986aca75eb522c756ddd808439740ca8
Daniel Petrov <dani@code6.ninja>
2025-12-11 12:00:42 +0200

    fixup! feat(multi-org-moderators): add moderator organizations to the User model

--- server/migrations/20251210094622-add-moderator-organizations.js
+++ server/migrations/20251210094622-add-moderator-organizations.js
@@ -4,19 +4,12 @@ const Sequelize = require('sequelize')
 module.exports = {
   async up (queryInterface) {
     await queryInterface.addColumn('Users', 'moderatorOrganizations', {
-      type: Sequelize.ARRAY(Sequelize.STRING),
+      type: Sequelize.TEXT,
       allowNull: true
-    })
-
-    await queryInterface.addIndex('Users', {
-      fields: ['moderatorOrganizations'],
-      using: 'GIN',
-      name: 'users_moderator_organizations_gin'
     })
   },
 
   async down (queryInterface) {
     await queryInterface.removeColumn('Users', 'moderatorOrganizations')
-    await queryInterface.removeIndex('Users', 'users_moderator_organizations_gin')
   }
 }
